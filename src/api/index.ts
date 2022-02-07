17c34ba10136b376d8949855c02533c7f7487d85
HarryXiong24 <harryxiong24@gmail.com>
2022-02-07 14:43:19 +0800

    fix: fix weather-search API

--- src/api/index.ts
+++ src/api/index.ts
@@ -222,6 +222,7 @@ export const reqIP = () => {
   return new Promise<any>(function (resolve, reject) {
     promise.then(function (response) {
       // 成功了调用resolve()
+      resolve(response)
     }).catch(function (error) {
       //失败了调用reject()
       reject(error)
